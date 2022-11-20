d5d7878ea6647c566d1ee444cb6da4de630ffa81
alisselisss <talisa220903@mail.ru>
2022-11-20 18:40:23 +0300

    dz_2

--- components/Button/Button.jsx
+++ components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import css from './Button.module.css'
 
-export const Button = ({title, children}) => {
+export const Button = ({title, children, type, onClick}) => {
   if (!children)
-      return <button className={css.button}>{title}</button>;
-  return <button className={css.button}>{children}</button>;
+      return <button type={type} onClick={onClick} className={css.button}>{title}</button>;
+  return <button type={type} onClick={onClick} className={css.button}>{children}</button>;
 }
